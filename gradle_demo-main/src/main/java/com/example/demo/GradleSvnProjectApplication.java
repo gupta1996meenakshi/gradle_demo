2109f9f96f5298753f9101d984a8e35d174506ca
gupta1996meenakshi <80736409+gupta1996meenakshi@users.noreply.github.com>
2021-07-22 12:03:49 +0530

    main

--- gradle_demo-main/src/main/java/com/example/demo/GradleSvnProjectApplication.java
+++ gradle_demo-main/src/main/java/com/example/demo/GradleSvnProjectApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GradleSvnProjectApplication {
 
-	public static void main(String[] args) {
+// 	public static void main(String[] args) {
 		SpringApplication.run(GradleSvnProjectApplication.class, args);
 		System.out.println("Hello From GradleSvnProjectApplication");
 	}
